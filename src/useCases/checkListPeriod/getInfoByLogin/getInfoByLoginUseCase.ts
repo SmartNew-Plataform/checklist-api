cc5bac04dedaeaff16e701102788244a39f9d23c
robsonsousa66 <robsonsousasousa66@gmail.com>
2023-08-14 10:04:19 -0300

    path: return img

--- src/useCases/checkListPeriod/getInfoByLogin/getInfoByLoginUseCase.ts
+++ src/useCases/checkListPeriod/getInfoByLogin/getInfoByLoginUseCase.ts
@@ -1,12 +1,18 @@
 import { Client } from 'basic-ftp'
 import { env } from '@/env'
+import path from 'path'
+import fs from 'fs'
 import IUseCase from '../../../models/IUseCase'
 import ICheckListPeriodRepository from '../../../repositories/ICheckListPeriodRepository'
 import IGetInfoByLoginRequestDTO from './IGetInfoByLoginRequestDTO'
 import IGetInfoByLoginResponseDTO from './IGetInfoByLoginResponseDTO'
+import IFTPService from '@/services/IFTPService'
 
 export default class GetInfoByLoginUseCase implements IUseCase {
-  constructor(private checkListPeriodRepository: ICheckListPeriodRepository) {}
+  constructor(
+    private checkListPeriodRepository: ICheckListPeriodRepository,
+    private FTPService: IFTPService,
+  ) {}
 
   async execute(data: IGetInfoByLoginRequestDTO) {
     const client = new Client()
@@ -30,19 +36,31 @@ export default class GetInfoByLoginUseCase implements IUseCase {
 
         const fileList = await client.list()
 
-        const fileInfo = fileList.map((fileItem) => {
+        const fileInfoPromises = fileList.map(async (fileItem) => {
+          const filePath = path.join(remotePath, fileItem.name)
+          const localTempPath = path.join(__dirname, fileItem.name)
+
+          await this.FTPService.download(client, filePath, localTempPath)
+
+          const fileDataBuffer = await fs.readFileSync(localTempPath)
+          const fileDataBase64 = fileDataBuffer.toString('base64')
+
+          await fs.unlinkSync(localTempPath)
+
           return {
             file: `https://www.smartnewsystem.com.br/sistemas/_lib/img/checkList/task_${item.id}/${fileItem.name}`,
-            base64: '',
+            base64: fileDataBase64,
           }
         })
+
+        const fileInfo = await Promise.all(fileInfoPromises)
 
         response.push({
           id: item.id,
           branchId: item.id_filial || 0,
           productionRegisterId: item.id_registro_producao || 0,
           checkListItemId: item.id_item_checklist || 0,
-          img: JSON.stringify(fileInfo),
+          img: fileInfo,
           statusItem: item.status_item || 0,
           statusNC: item.status_item_nc || 0,
           logDate: item.log_date,
@@ -55,7 +73,7 @@ export default class GetInfoByLoginUseCase implements IUseCase {
           branchId: item.id_filial || 0,
           productionRegisterId: item.id_registro_producao || 0,
           checkListItemId: item.id_item_checklist || 0,
-          img: '',
+          img: [],
           statusItem: item.status_item || 0,
           statusNC: item.status_item_nc || 0,
           logDate: item.log_date,
