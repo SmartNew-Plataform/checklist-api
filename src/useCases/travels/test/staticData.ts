c29ec0599cc7e5b052a18b86ad8b81e1b87c190f
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2024-01-17 15:30:30 -0300

    docs: update static travels data

--- src/useCases/travels/test/staticData.ts
+++ src/useCases/travels/test/staticData.ts
@@ -1,26 +1,51 @@
+type LocationType = {
+  latitude: number
+  longitude: number
+}
+
 type TravelType = {
   id: number
   date: Date
   destination: string
+  destinationCoords: LocationType
   user: string
 
   status: 'due' | 'progress' | 'paused' | 'stopped' | 'finished'
   pauses: {
     started: Date
     ended: Date | null
-    pausedLocation: string
-    resumedLocation: string | null
+    pausedLocation: LocationType
+    resumedLocation: LocationType | null
   }[]
   distanceTraveled: number
-  startLocation: string | null
-  currentLocation: string | null
+  startLocation: LocationType | null
+  currentLocation: LocationType | null
 }
 
 export const travelsData: TravelType[] = [
   {
+    id: 902,
+    date: new Date('2024-01-15'),
+    destination: 'Los Angeles - CA',
+    destinationCoords: {
+      latitude: 34.0194,
+      longitude: -118.411,
+    },
+    user: 'bruno.matias',
+    startLocation: null,
+    status: 'due',
+    pauses: [],
+    distanceTraveled: 0,
+    currentLocation: null,
+  },
+  {
     id: 903,
     date: new Date('2024-01-07'),
     destination: 'Uberlândia - MG',
+    destinationCoords: {
+      latitude: -18.9113,
+      longitude: -48.2622,
+    },
     user: 'bruno.matias',
     startLocation: null,
     status: 'due',
@@ -32,6 +57,10 @@ export const travelsData: TravelType[] = [
     id: 904,
     date: new Date('2024-01-10'),
     destination: 'Rio de janeiro - rj',
+    destinationCoords: {
+      latitude: -22.9035,
+      longitude: -43.2096,
+    },
     user: 'bruno.matias',
     startLocation: null,
     status: 'due',
@@ -43,6 +72,10 @@ export const travelsData: TravelType[] = [
     id: 905,
     date: new Date('2024-02-03'),
     destination: 'Sao paulo - sp',
+    destinationCoords: {
+      latitude: -23.5489,
+      longitude: -46.6388,
+    },
     user: 'bruno.matias',
     startLocation: null,
     status: 'due',
@@ -54,6 +87,10 @@ export const travelsData: TravelType[] = [
     id: 906,
     date: new Date('2024-02-09'),
     destination: 'fortaleza - ce',
+    destinationCoords: {
+      latitude: -3.71839,
+      longitude: -38.5434,
+    },
     user: 'bruno.matias',
     startLocation: null,
     status: 'due',
