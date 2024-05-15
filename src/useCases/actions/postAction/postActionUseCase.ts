a430bb0f8fc5b77cfcc1480609200ed580a6ca93
robsonsousa66 <robsonsousasousa66@gmail.com>
2024-05-15 14:15:16 -0300

    feat: test

--- src/useCases/actions/postAction/postActionUseCase.ts
+++ src/useCases/actions/postAction/postActionUseCase.ts
@@ -17,39 +17,33 @@ export default class PostActionUseCase implements IUseCase {
 
     console.log(data)
 
-    try {
-      const group = await this.actionGroupRepository.create({
-        id_cliente: data.user.id_cliente,
-        numero: 1,
-        data_fim: new Date(data.endDate),
-        data_concluida: data.dueDate ? new Date(data.dueDate) : null,
-        data_inicio: new Date(data.startDate),
-        descricao: data.title,
-        descricao_acao: data.description,
-        // id_registro_producao: data.checklistId,
-        id_checklist: data.checklistId,
-        responsavel: data.responsible,
-      })
-      const inserted = await this.actionRepository.create({
-        id_grupo: group.id,
-        data_fim: new Date(data.endDate),
-        data_fechamento: data.dueDate ? new Date(data.dueDate) : null,
-        data_inicio: new Date(data.startDate),
-        descricao: data.title,
-        descricao_acao: data.description,
-        id_item: data.checklistPeriodId,
-        // id_registro_producao: data.checklistId,
-        id_checklist: data.checklistId,
-        responsavel: data.responsible,
-      })
+    const group = await this.actionGroupRepository.create({
+      id_cliente: data.user.id_cliente,
+      numero: 1,
+      data_fim: new Date(data.endDate),
+      data_concluida: data.dueDate ? new Date(data.dueDate) : null,
+      data_inicio: new Date(data.startDate),
+      descricao: data.title,
+      descricao_acao: data.description,
+      // id_registro_producao: data.checklistId,
+      id_checklist: data.checklistId,
+      responsavel: data.responsible,
+    })
+    const inserted = await this.actionRepository.create({
+      id_grupo: group.id,
+      data_fim: new Date(data.endDate),
+      data_fechamento: data.dueDate ? new Date(data.dueDate) : null,
+      data_inicio: new Date(data.startDate),
+      descricao: data.title,
+      descricao_acao: data.description,
+      id_item: data.checklistPeriodId,
+      // id_registro_producao: data.checklistId,
+      id_checklist: data.checklistId,
+      responsavel: data.responsible,
+    })
 
-      return {
-        ...inserted,
-      }
-    } catch (error) {
-      throw CustomError.internalServerError(
-        'Erro ao salvar dados ' + JSON.stringify(error),
-      )
+    return {
+      ...inserted,
     }
   }
 }
